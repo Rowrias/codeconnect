d68d02708d016a8270a9183469f4072aff341076
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2025-08-29 09:04:37 -0300

    04.Criando requisições
    - useEffect
    - Requisição para API
    - Props

--- src/componentes/Card/index.jsx
+++ src/componentes/Card/index.jsx
@@ -1,42 +1,40 @@
 import './styles.css';
-import Capa from './assets/capa.png';
 import Code from './assets/code.svg';
 import Share from './assets/share.svg';
 import Chat from './assets/chat.svg';
-import Icone from './assets/Ellipse 1.png';
 
-export default function Card() {
+export default function Card({id, imagemUrl, titulo, resumo, linhasDeCodigo,compartilhamentos, comentarios, usuario}) {
     return(
-        <article className='card'>
+        <article className='card' id={id}>
             <div className='card__imagem'>
-                <img src={Capa} alt="imagem do post" />
+                <img src={imagemUrl} alt="imagem do post" />
             </div>
             <div className='card__conteudo'>
                 <div className='conteudo__texto'>
-                    <h3>Título do post</h3>
-                    <p>Resumo do post</p>
+                    <h3>{titulo}</h3>
+                    <p>{resumo}</p>
                 </div>
 
                 <div className='conteudo__rodape'>
                     <ul>
                         <li>
                             <img src={Code} alt="códigos" />
-                            100
+                            {linhasDeCodigo}
                         </li>
                         <li>
                             <img src={Share} alt="compartilhamentos" />
-                            12
+                            {compartilhamentos}
                         </li>
                         <li>
                             <img src={Chat} alt="comentários" />
-                            10
+                            {comentarios}
                         </li>
                     </ul>
                 </div>
 
                 <div className='rodape__usuario'>
-                    <img src={Icone} alt="imagem do usuário" />
-                    @Moni
+                    <img src={usuario.imagem} alt="imagem do usuário" />
+                    {usuario.nome}
                 </div>
             </div>
         </article>
